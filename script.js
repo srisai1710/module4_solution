344a515d2ad47aa054a2b31c9264c05b52edc309
srisai1710 <138852957+srisai1710@users.noreply.github.com>
2023-07-25 01:10:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var x=[];
+x[0]="Sai";
+x[1]="Jay";
+x[2]="Naresh";
+x[3]="Srinu";
+x[4]="John";
+x[5]="Giri";
+x[6]="Jany";
+x[7]="Shiva";
+x[8]="Jaiswal";
+x[9]="Mani";
+for(var i = 0 ; i < x.length ; i++) {
+    if("j"&&"J"==x[i][0]){
+        console.log("Goodbye "+x[i]);
+    }
+    else{
+        console.log("Hello "+x[i]);
+    } 
+}
